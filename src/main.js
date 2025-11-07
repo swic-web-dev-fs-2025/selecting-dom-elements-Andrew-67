6631cba13b96d1751102165f2d82ae5b3654dee1
Reece, Andrew <andrew.reece@student.swic.edu>
2025-11-07 15:55:53 -0600

    Located and Altered Price

--- src/main.js
+++ src/main.js
@@ -12,3 +12,12 @@ console.log(`Number of internal links: ${internalLinks.length}`);
 
 const inputsStyles = document.querySelectorAll("input");
 inputsStyles.forEach((input) => {input.style.outline = "2px solid gold";});
+
+const cards = document.querySelectorAll(".card");
+cards.forEach((card, index) => {
+  card.setAttribute("data-viewed", "true");
+  console.log(`Card ${index + 1} viewed status: ${card.getAttribute("data-viewed")}`);
+});
+const productChange = document.querySelector('[data-product-id="2"] .price');
+productChange.textContent = "$24.99";
+console.log(`Updated price for product 2: ${productChange.textContent}`);
