59523502c45b767cd26d082b0552f0594d42d9d7
Reece, Andrew <andrew.reece@student.swic.edu>
2025-11-07 11:44:10 -0600

    Locate and Log Functional Navlink

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import "./style.css";
 
 const mainHeader = document.getElementById("main-header");
-console.log(mainHeader);
+console.log(mainHeader.textContent);
+
+const links = document.querySelectorAll(".navbar");
+console.log(`Number of links in navbar: ${links.length}`);
+
